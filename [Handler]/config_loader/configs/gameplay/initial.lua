69c49334446925116a7e2fb87ab5d254ef3f0956
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-01-25 07:19:31 +0300

    Organized into generic categories

--- [Handler]/config_loader/configs/gameplay/initial.lua
+++ [Handler]/config_loader/configs/gameplay/initial.lua
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------
 --[[ Resource: Config Loader
-     Script: configurations: initial.lua
+     Script: configurations: config_initial.lua
      Author: vStudio
      Developer(s): Tron
      DOC: 25/01/2022
